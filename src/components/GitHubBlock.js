67820833a93a3bcf75f4800d7cabbd7ed11b558d
Chad Ellison <chad.ellison0123@gmail.com>
2019-11-13 15:03:05 -0600

    add github block

--- src/components/GitHubBlock.js
+++ src/components/GitHubBlock.js
@@ -1,24 +1,44 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import GitHubProfile from './GitHubProfile';
 
-const GitHubBlock = () => {
+// fetchGitHubData = (userName) => {
+//   axios
+//    .get("https://api.github.com/users/jab115")
+//    .then(({ data }) => {
+//      console.log('we did it')
+//      // updateGitHubData(data)
+//      return data
+//    }).catch((e) => {
+//      return {};
+//      // updateGitHubData({})
+//    });
+// }
+const GitHubBlock = ({gitHubUserName}) => {
+  const [gitHubData, updateGitHubData] = useState();
+  // const [ newTodoLabel, setNewTodoLabel] = useState("");
+
+  useEffect(() => {
+    axios
+     .get(`https://api.github.com/users/jab115`)
+     .then(({ data }) => {
+       console.log('we did it')
+       updateGitHubData(data)
+       console.log(data)
+     }).catch((e) => {
+       updateGitHubData({})
+     });
+    // let data = fetchGitHubData(gitHubUserName)
+    // console.log(data, '(((((((((((())))))))))))')
+    // updateGitHubData(data)
+  }, []);
   return (
       <div className="tweets block">
           <h2 className="titular">
-              <span className="icon zocial-twitter"></span>LATEST TWEETS
+              <span className="icon zocial-GitHubBlock"></span>GitHub
           </h2>
           <div className="tweet first">
-              <p>
-                  Ice-cream trucks only play music when out of ice-cream. Well
-                  played dad. On{" "}
-                  <a className="tweet-link" href="#17">
-                      @Quora
-                  </a>
-              </p>
-              <p>
-                  <a className="time-ago scnd-font-color" href="#18">
-                      3 minutes ago
-                  </a>
-              </p>
+            {gitHubData && gitHubData.login ? <GitHubProfile gitHubData={gitHubData} /> : <form onSubmit={console.log('yo')}>Add your gihub Profile</form>}
           </div>
           <div className="tweet">
               <p>
