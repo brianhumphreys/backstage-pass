5ff0b8d71272079aea35dbd6937afb8a650445e8
Chad Ellison <chad.ellison0123@gmail.com>
2019-11-13 16:16:21 -0600

    github card

--- src/components/GitHubProfile.js
+++ src/components/GitHubProfile.js
@@ -1,34 +1,23 @@
 import React from "react";
 
 const GitHubProfile = ({gitHubData}) => {
-  // useEffect(() => {
-  //   axios
-  //    .get(`https://api.github.com/users/jab115`)
-  //    .then(({ data }) => {
-  //      console.log('we did it')
-  //      updateGitHubData(data)
-  //      console.log(data)
-  //    }).catch((e) => {
-  //      updateGitHubData({})
-  //    });
-  //   // let data = fetchGitHubData(gitHubUserName)
-  //   // console.log(data, '(((((((((((())))))))))))')
-  //   // updateGitHubData(data)
-  // }, []);
   return (
-    <div>
-      <p>
-        Ice-cream trucks only play music when out of ice-cream. Well
-        played dad. On{" "}
-      <a className="tweet-link" href="#17">
-        @Quora
-      </a>
-      </p>
-      <p>
-      <a className="time-ago scnd-font-color" href="#18">
-        3 minutes ago
-      </a>
-      </p>
+    <div className="icon fontawesome-heart-empty scnd-font-color">
+      <div>
+        UserName: {gitHubData.login}
+      </div>
+      <div>
+        Name: {gitHubData.name}
+      </div>
+      <div>
+        Repositories: {gitHubData.public_repos}
+      </div>
+      <div>
+        <a className="" href={'https://github.com/' + gitHubData.login}
+          target="_blank">
+          View Profile
+        </a>
+      </div>
     </div>
   );
 }
