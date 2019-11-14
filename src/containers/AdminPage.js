92b2db3b5844e3693a1fa3c1405b08a154ede11c
Jonathan Blow <jonathan.blow95@gmail.com>
2019-11-14 07:44:37 -0600

    Admin page styling and functionality

--- src/containers/AdminPage.js
+++ src/containers/AdminPage.js
@@ -4,42 +4,42 @@ import { getEmployees} from "../store/storeFunctions";
 const AdminPage = () => {
 
     return (
-      <div className="account block">
-          Here is my admin page
-
-          <table >
-              <thead >
-              <tr>
-                  <th>Name</th>
-                  <th>E-mail</th>
-                  <th>Bio</th>
-                  <th>Rank</th>
-                  <th>Contact</th>
-              </tr>
-              </thead>
-              <tbody>
-              {getEmployees().map(e => {
-                  return <tr>
-                      <td>
-                          {e.employee.name}
-                      </td>
-                      <td>
-                          {e.employee.email}
-                      </td>
-                      <td>
-                          {e.employee.bio}
-                      </td>
-                      <td>
-                          {87}
-                      </td>
-                      <td>
-                          <a>Send Email</a>
-                      </td>
+      <div className="admin-table-container">
+          <div className="admin-table">
+              <table className="table">
+                  <thead >
+                  <tr>
+                      <th>Name</th>
+                      <th>E-mail</th>
+                      <th>Bio</th>
+                      <th>Rank</th>
+                      <th>Contact</th>
                   </tr>
-              })}
+                  </thead>
+                  <tbody>
+                  {getEmployees().map(e => {
+                      return <tr className="admin-table-row">
+                          <td>
+                              {e.name}
+                          </td>
+                          <td>
+                              {e.email}
+                          </td>
+                          <td>
+                              {e.about}
+                          </td>
+                          <td>
+                              <strong>{87}</strong>
+                          </td>
+                          <td>
+                              <button className="button" onClick={() => window.open(`mailto:test@${e.email}`)}>I'm Interested!</button>
+                          </td>
+                      </tr>
+                  })}
 
-              </tbody>
-          </table>
+                  </tbody>
+              </table>
+          </div>
       </div>
   );
 };
