60cc21963d7fe1b098aaf4d0fee9a1c165639cf0
Jonathan Blow <jonathan.blow95@gmail.com>
2019-11-14 08:00:04 -0600

    sorting on the admin page

--- src/containers/AdminPage.js
+++ src/containers/AdminPage.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { getEmployees} from "../store/storeFunctions";
 
+const getScore = (employee) =>{
+    return employee.skills.map(skill => skill.level).reduce((a, b) => a + b, 0)
+}
+
+const compare = (employee1, employee2) => {
+
+ const score1 = employee1.props.children[3].props.children.props.children
+ const score2 = employee2.props.children[3].props.children.props.children
+
+     if (score1 === score2){
+         return 0
+     }else if (score1 > score2){
+         return -1
+     } else {
+         return 1
+     }
+}
+
 const AdminPage = () => {
 
     return (
@@ -29,13 +47,13 @@ const AdminPage = () => {
                               {e.about}
                           </td>
                           <td>
-                              <strong>{87}</strong>
+                              <strong>{getScore(e)}</strong>
                           </td>
                           <td>
                               <button className="button" onClick={() => window.open(`mailto:test@${e.email}`)}>I'm Interested!</button>
                           </td>
                       </tr>
-                  })}
+                  }).sort(compare)}
 
                   </tbody>
               </table>
