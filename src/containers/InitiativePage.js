bdd35413872a12ad3288e272370bb9e1aef8d2f8
Brian Humphreys <brianhumphreys@Brians-MacBook-Pro.local>
2019-11-14 12:39:51 -0600

    yuuuuus

--- src/containers/InitiativePage.js
+++ src/containers/InitiativePage.js
@@ -2,12 +2,18 @@ import React from "react";
 import RadarChartBlock from "../components/RadarChartBlock/RadarChartBlock";
 import initiatives from "../mockData/initiatives";
 import InitiativeBlock from "../components/InitiativeBlock";
-import { signedInEmployee } from "../mockData/employees";
+import { getSignedInUser, getEmployees } from "../store/storeFunctions";
 
 const InitiativePage = () => {
   const location = window.location.href.split('/');
   const index = initiatives.findIndex(initiative => {
     return initiative.path === location[location.length-1];
+  });
+
+  const current = getSignedInUser();
+  const employees = getEmployees();
+  const employeeIndex = employees.findIndex(employee => {
+    return employee.name === current.name;
   });
 
   return (
@@ -17,7 +23,7 @@ const InitiativePage = () => {
           <InitiativeBlock account={initiatives[index]}/>
         </div>
         <div className="middle-container container">
-          <RadarChartBlock id="skills" account={signedInEmployee} initiative={initiatives[index]} comparing={true} />
+          <RadarChartBlock id="skills" account={employees[employeeIndex]} initiative={initiatives[index]} comparing={true} />
         </div>
         <div className="right-container container">
         </div>
